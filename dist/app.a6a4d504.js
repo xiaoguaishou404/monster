d5a05d192cf1ea87699d545e0b4993a93231c0e8
shenshen <2054376909@qq.com>
2022-07-06 12:36:38 +0800

    把.cache目录和dist目录添加gitignore

--- dist/app.a6a4d504.js
+++ dist/app.a6a4d504.js
@@ -11450,7 +11450,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "4609" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "7451" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
