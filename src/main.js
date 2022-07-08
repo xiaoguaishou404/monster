d356d90c24e5278b9558b88cd38bd95dd49da8f1
shenshen <xiaoguaishou404@qq.com>
2022-07-08 16:20:47 +0800

    增加mButton组件的click事件逻辑的单元测试

--- src/main.js
+++ src/main.js
@@ -15,6 +15,9 @@ new Vue({
     render: h => h(App)
 })
 import chai from 'chai'
+import spies from 'chai-spies'
+chai.use(spies);
+
 const expect = chai.expect
 // 单元测试
 {
@@ -46,21 +49,7 @@ const expect = chai.expect
     vm.$el.remove()
     vm.$destroy()
 }
-// {
-//     const Constructor = Vue.extend(Button)
-//     const vm = new Constructor({
-//         propsData: {
-//             icon: 'settings',
-//             loading: true
-//         }
-//     })
-//     vm.$mount()
-//     let useElement = vm.$el.querySelector('use')
-//     let href = useElement.getAttribute('xlink:href')
-//     expect(href).to.eq('#i-loading')
-//     vm.$el.remove()
-//     vm.$destroy()
-// }
+
 //   {
 //     const div = document.createElement('div')
 //     document.body.appendChild(div)
@@ -78,19 +67,35 @@ const expect = chai.expect
 //     vm.$el.remove()
 //     vm.$destroy()
 //   }
-//   {
-//     // mock
-//     const Constructor = Vue.extend(Button)
-//     const vm = new Constructor({
-//       propsData: {
-//         icon: 'settings',
-//       }
-//     })
-//     vm.$mount()
-//     vm.$on('click', function () {
-//       expect(1).to.eq(1)
-//     })
-//     // 希望这个函数被执行
-//     let button = vm.$el
-//     button.click()
-//   }
+
+{
+    // mock
+    const Constructor = Vue.extend(mButton)
+    const vm = new Constructor({
+        propsData: {
+            icon: '#icon-setting',
+        }
+    })
+    vm.$mount()
+    let eventType
+    let spy = chai.spy((e) => { eventType = e.type })
+    // 这是使用dom原生的事件
+    vm.$el.onclick = spy
+    vm.$el.click()
+    // 也可以使用下面的vue方法触发事件，但不能叉用原生和vue的绑定和触发，那样是肯定不会触发的，因为没有联系起来
+    // 建议使用上面的，因为我们的mButton使用native绑定的是原生click事件
+
+    // vm.$on('click', spy)
+    // vm.$emit("click")
+
+
+    expect(spy).to.have.been.called()
+    expect(eventType).to.eq("click")
+    vm.$destroy()
+    // 这个库的间谍我觉得并不好，他只是判断spy有没有触发而已
+    // 我可能还需要判断函数是不是click事件触发的
+    // 所以使用eventType做了一些改进
+
+    // 为什么老师的可以vm.$on和.click()可以一起使用，因为他的mButton内部和我设计的不一样
+    // 他确实需要这样，在父级添加$on，子级再原生click触发
+}
