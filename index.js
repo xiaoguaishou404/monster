54f974a0b089844f33f7e59d90154c36b4bf812f
shenshen <xiaoguaishou404@qq.com>
2022-07-21 15:10:19 +0800

    发布0.0.6版本至npm

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 import mButton from "./src/components/mButton.vue"
 import mButtonGroup from "./src/components/mButtonGroup.vue"
 import mIcon from "./src/components/mIcon.vue"
-
+import mInput from "./components/mInput.vue"
+import mRow from "./components/mRow.vue"
+import mCol from "./components/mCol.vue"
+import mLayout from './components/mLayout.vue'
+import mHeader from './components/mHeader.vue'
+import mSider from './components/mSider.vue'
+import mContent from './components/mContent.vue'
+import mFooter from './components/mFooter.vue'
+import mToast from './components/mToast.vue'
+import plugin from './components/plugin.js'
+import mPopover from './components/mPopover.vue'
+import mTabsBody from './components/mTabs-body.vue'
+import mTabsHead from './components/mTabs-head.vue'
+import mTabsItem from './components/mTabs-item.vue'
+import mTabsPane from './components/mTabs-pane.vue'
+import mTabs from './components/mTabs.vue'
+import mCollapse from './components/mCollapse.vue'
+import mCollapseItem from './components/mCollapseItem.vue'
 // Vue.component('m-button', mButton)
 // Vue.component('m-button-group', mButtonGroup)
 // Vue.component('m-icon', mIcon)
 
-export { mButton, mButtonGroup, mIcon }
+export {
+    mButton, mButtonGroup, mIcon, mInput, mRow, mCol, mLayout, mHeader, mSider, mContent, mFooter, mToast, plugin, mPopover, mTabsBody, mTabsHead, mTabsItem, mTabsPane, mTabs, mCollapse, mCollapseItem
+}
